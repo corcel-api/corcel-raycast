5811098ca7522a9a5207515f307c902eb79dca2a
Akin Aguda <lordtitan7@gmail.com>
2024-04-19 12:15:25 +0100

    step is handled and documented better

--- chat/src/lib/image/model-step.ts
+++ chat/src/lib/image/model-step.ts
@@ -1,9 +1,7 @@
-import { inLerp, lerp } from "../math";
+import { lerp } from "../math";
 import { ImageGenerationModel } from "./types";
 
 type Range = [number, number];
-
-const CLIIENT_RANGE: Range = [1, 7];
 
 const ENGINE_TO_RANGE_MAP: Record<ImageGenerationModel, Range> = {
   dreamshaper: [6, 12],
@@ -17,22 +15,7 @@ export const fromClientRangeToModelRange = (clientRangeValue: number, model: Ima
     lerp(
       engineRange[0],
       engineRange[1],
-      inLerp(
-        CLIIENT_RANGE[0],
-        CLIIENT_RANGE[1],
-        Math.max(Math.min(clientRangeValue, CLIIENT_RANGE[1]), CLIIENT_RANGE[0]), // Clamps value
-      ),
-    ),
-  );
-};
-
-export const toClientRangeFromModelRange = (modelRangeValue: number, model: ImageGenerationModel) => {
-  const engineRange = ENGINE_TO_RANGE_MAP[model];
-  return Math.round(
-    lerp(
-      CLIIENT_RANGE[0],
-      CLIIENT_RANGE[1],
-      inLerp(engineRange[0], engineRange[1], Math.max(Math.min(modelRangeValue, engineRange[1]), engineRange[0])), // Clamps value
+      Math.min(Math.max(clientRangeValue, 0), 1), // Clamps value
     ),
   );
 };
